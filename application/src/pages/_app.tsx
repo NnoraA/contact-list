5833ee88b89f2d1259e0e62f0ff8d9eb5808e631
Nóra Hornyák <pappnoori@gmail.com>
2023-01-22 17:29:20 +0100

    feat(-app.tsx): add QueryClient

--- application/src/pages/_app.tsx
+++ application/src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@application/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@application/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 20 * 1000,
+            retry: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
