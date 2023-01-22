37bc3055b84c2a886ac895de41915f3badb7f2cf
Nóra Hornyák <pappnoori@gmail.com>
2023-01-22 17:22:55 +0100

    chore(): add cors

--- api/index.ts
+++ api/index.ts
@@ -4,12 +4,15 @@ dotenv.config();
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import { contactsRoutes } from "./routes/contacts.route";
+import cors from "cors";
 
 export const prisma = new PrismaClient();
 
 const port = process.env.PORT;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
