fefc117a17a74c2f60721a2d88d6357ebd2da67b
Nóra Hornyák <pappnoori@gmail.com>
2023-01-25 18:07:14 +0100

    feat(next.config.js): redirect path

--- application/next.config.js
+++ application/next.config.js
@@ -3,7 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     API_URL: process.env.API_URL
-  }
+  },
+  images: {
+    domains: ['localhost'],
+},
+async redirects() {
+  return [
+    {
+      source: '/',
+      destination: '/contacts',
+      permanent: true,
+    },
+  ]
+},
 }
 
 module.exports = nextConfig
